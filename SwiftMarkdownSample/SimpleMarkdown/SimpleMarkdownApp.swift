9cfd742a4aba0245b4dcc7cdd29fa67f3c860115
Kuriyama <paprapapra@gmail.com>
2023-07-01 13:28:30 +0900

    Add sample project and add "SimpleMarkdown" target.

---
+++ SwiftMarkdownSample/SimpleMarkdown/SimpleMarkdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleMarkdownApp.swift
+//  SimpleMarkdown
+//
+//  Created by Toru Kuriyama on 2023/07/01.
+//
+
+import SwiftUI
+
+@main
+struct SimpleMarkdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
